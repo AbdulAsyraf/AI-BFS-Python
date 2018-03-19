13831521a868e77795cba39559fe2fd615b2daf0
AbdulAsyraf <abdulasyrafah@gmail.com>
2018-03-19 21:46:39 +0800

    first working random input version

--- test.py
+++ test.py
@@ -5,13 +5,29 @@ color = ["red", "blue", "yellow", "green", "orange"]
 total = []
 loc = []
 
-def search(i, total):
+def setup(i, total):
     if i < 7:
-        return total[i+7:] + total[:i+7]
+        return total[i+8:] + total[:i+7]
     elif i > 7:
         return  total[i-7:] + total[:i-7]
     elif i == 7:
         return  total         
+
+def search(state, target, answer):
+    for i in range(1, 7):
+        for j in range(0, 3):
+            temp1 = state[j][7-i]
+            temp2 = state[j][7+i]
+            if temp1 == target:
+                answer.append(j)
+                answer.append(-i)
+                break
+            elif temp2 == target:
+                answer.append(j)
+                answer.append(i)
+                break
+        if len(answer) != 0:
+            break
 
 for i in range(0, 3):
     temp = color
@@ -38,6 +54,18 @@ print("test value is: %s" % test)
 work = [0,0,0]
 
 for i in range(0, 3):
-    work[i] = search(loc[i], total)
+    work[i] = setup(loc[i], total)
 
-print(work)
+answer = []
+search(work, test, answer)
+
+if answer[1] < 0:
+    part2 = " %i step(s) to the left"
+else:
+    part2 = " %i step(s) to the right"
+
+part1 = "Target found after searching from index %i"
+whole = part1 + part2
+
+answer[1] = abs(answer[1])
+print(whole % (loc[answer[0]], answer[1]))
