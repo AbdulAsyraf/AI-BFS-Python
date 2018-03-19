82b547eb415b4eded88a64246085ae1ee68c3390
AbdulAsyraf <abdulasyrafah@gmail.com>
2018-03-19 20:23:36 +0800

    update

--- test.py
+++ test.py
@@ -1,9 +1,17 @@
 from random import *
 
-color = [1, 2, 3, 4, 5]
+color = ["red", "blue", "yellow", "green", "orange"]
 
 total = []
 loc = []
+
+def search(i, total):
+    if i < 7:
+        return total[i+7:] + total[:i+7]
+    elif i > 7:
+        return  total[i-7:] + total[:i-7]
+    elif i == 7:
+        return  total         
 
 for i in range(0, 3):
     temp = color
@@ -12,19 +20,24 @@ for i in range(0, 3):
 
 print(total)
 
-start = randint(1,5)
-print("start: %i" % start)
+start = color[randint(0,4)]
+print("start: %s" % start)
 
 for j in range(0, len(total)):
     if total[j] == start:
-        print(j)
         loc.append(j)
 
-print(loc)
+print("Index with start color : %s" % loc)
 
 test = start
 while test == start:
-    test = randint(1, 5)
-    print("=")
+    test = color[randint(0, 4)]
 
-print("test: %i" % test)
+print("test value is: %s" % test)
+
+work = [0,0,0]
+
+for i in range(0, 3):
+    work[i] = search(loc[i], total)
+
+print(work)
