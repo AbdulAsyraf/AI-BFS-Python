491f2f7a2584b8ef1593b4dd5ddaa27d19100448
AbdulAsyraf <abdulasyrafah@gmail.com>
2018-03-23 01:56:25 +0800

    semi commented version

--- uninformed.py
+++ uninformed.py
@@ -1,6 +1,6 @@
-from random import *
+from random import *        #imports random functions
 
-def setup(i, total):
+def setup(i, total):        #splits the list and positions the initial state in the middle
     if i < 7:
         return total[i+8:] + total[:i+7]
     elif i > 7:
@@ -8,7 +8,7 @@ def setup(i, total):
     elif i == 7:
         return  total         
 
-def search(state, target, answer):
+def search(state, target, answer):      #expands the node and searches the child nodes from left to right
     for i in range(1, 7):
         temp1 = state[7-i]
         temp2 = state[7+i]
@@ -29,28 +29,28 @@ print("1 - BLUE")
 print("2 - YELLOW")
 print("3 - GREEN")
 print("4 - ORANGE")
-check = input("Please enter a number to search for the color: ")
+check = int(input("Please enter a number to search for the color: "))
 
 test = color[check]
 
 print("The color %s is chosen" % test)
 
-for i in range(0, 3):
+for i in range(0, 3):           #creating the search state
     temp = color
     shuffle(temp)
     total = total + temp
 
-print(total)
+print(total)                    #displaying the states
 
 start = test
 while start == test:
-    start = color[randint(0, 4)]
+    start = color[randint(0, 4)]        #assigning the starting color to be different than the value input by the user
 
 for j in range(0, len(total)):
     if total[j] == start:
         loc.append(j)
 
-startLoc = loc[randint(0, 2)]
+startLoc = loc[randint(0, 2)]           #choosing among the 3 positions to set as initial state
 
 print("Initial position of the funnel is underneath the color %s at index %i" % (start, startLoc))
 
